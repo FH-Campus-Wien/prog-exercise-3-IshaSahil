6bc7e70012457a411c8359be9791fcabe4b5f467
IshaSahil <114118020+IshaSahil@users.noreply.github.com>
2022-10-21 23:23:46 +0200

    Task 1

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,16 +1,55 @@
 package at.ac.fhcampuswien;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class App {
 
-    // Implement all methods as public static
+    //Implement all methods as public static
 
     public static void main(String[] args) {
-        // test your method implementations here
-        // make method calls
-        // print their results
-        // etc.
-    }
-}
+        //test your method implementation here
+        //make method calls
+        //print their results
+        //etc.
+
+        public static void oneMonthCalender(int numDays,int startDay){
+            //if bedingung mit modulo
+            //rechtsbündig,soutprintf ( %, ... ) oder andere Bedingung
+
+            int row;
+            int count = 1;
+            int column = startDay;
+
+
+            if ((numDays + startDay) % 7 != 0) {
+                row = (numDays / 7) + 1;
+
+            } else {
+                row = numDays / 7;
+            }
+            for (int i = 1; i < startDay; i++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 0; j < row; j++) {
+                while (column <= 7) {
+                    System.out.printf("%2d ", count);
+                    if (count == numDays) {
+                        System.out.println();
+                        return;
+                    }
+                    column++;
+                    count++;
+                }
+                column = 1;
+                System.out.println();
+            }
+        }
+
+
+
+
+
+
